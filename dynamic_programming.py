ff89ba1842065c11815dc84ea38361c3216eff58
Erik Orsolin <depaulaerik670@gmail.com>
2023-04-29 15:19:19 -0300

    mudança no nome das variáveis

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -1,28 +1,26 @@
 # esse código pretende resolver o problema da mochila (knapsack problem) com programação dinâmica
 import time
+from random import randint
 
-def knapsack(W, wt, val, n):
+def knapsack(p_max, p_items, value_items, n_items):
     inicio = time.time()
-    K = [[0 for x in range(W+1)] for x in range(n+1)]
-    for i in range(n+1):
-        for w in range(W+1):
-            if i == 0 or w == 0: # caso base
-                K[i][w] = 0
-            elif wt[i-1] <= w: # se o peso do item é menor ou igual ao peso da mochila
-                K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]], K[i-1][w]) # pega o maior valor entre colocar o item na mochila e não colocar
+    K = [[0 for x in range(p_max+1)] for x in range(n_items+1)]
+    for i in range(n_items+1):
+        for p in range(p_max+1):
+            if i == 0 or p == 0: # caso base
+                K[i][p] = 0
+            elif p_items[i-1] <= p_max: # se o peso do item é menor ou igual ao peso da mochila
+                K[i][p] = max(value_items[i-1] + K[i-1][p_max-p_items[i-1]], K[i-1][p_max]) # pega o maior valor entre colocar o item na mochila e não colocar
             else: # se o peso do item é maior que o peso da mochila
-                K[i][w] = K[i-1][w] # não coloca o item na mochila
+                K[i][p] = K[i-1][p] # não coloca o item na mochila
     fim = time.time()
     tempo = fim - inicio
-    return [K[n][W], tempo]
+    utilidade = K[n_items][p_max]
+    return (utilidade, tempo)
 
-# w = peso da mochila
-# wt = vetor com os pesos dos itens
-# val = vetor com os valores dos itens
-# n = número de itens
+n = 100
 
-r = knapsack(180, [10, 20, 30, 50, 65, 70, 80, 95], [60, 100, 120, 140, 700, 800, 850, 855], 8)
+
+r = knapsack(400, [randint(20, 600) for i in range(n)], [randint(900, 3000) for i in range(n)], n)
 print(r[0])
-print("Tempo de execução: {} ms".format(r[1]*10**3))
-
-
+print("Tempo de execução: {} seg".format(r[1]))
